1d69a0d315f0a13a6c158dd19bbc9802ee0debfa
maniizzle <olamideonakoya1@gmail.com>
2023-03-04 16:33:20 +0100

    setup circle ci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="manalif/udacityproject4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl create deploy machinelearningapp --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/machinelearningapp 8000:80 
